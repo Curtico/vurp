42bd16f7c1bf1369939c259b7a7bff57777edcdd
Joshua Hartzfeld <102620766+JoshuaHartz@users.noreply.github.com>
2023-11-16 14:32:14 -0500

    Add files via upload

--- detection.py
+++ detection.py
@@ -1,5 +1,13 @@
 from pwn import *
 
+context.update(
+    arch="amd64",
+    endian="little",
+    log_level="warning",
+    os="linux",
+    #terminal=["tmux", "split-window", "-h", "-p 65"]
+    terminal=["st"]
+)
 
 def scan(binary):
     e = ELF(f"./{binary}")
@@ -7,14 +15,17 @@ def scan(binary):
     #r = ROP(e)
 
     #-- overflow time --#
-    p.sendline(cyclic(1000))
+    p.sendline(cyclic(3000))
+    p.wait()
+
+    if p.poll() and p.poll() < 0:
+        detect_overflow()
     p.interactive()
 
 
 
 
 
-
 def detect_overflow():
-    pass
+    print('Overflowed')
 #detecting overflow types soontm
