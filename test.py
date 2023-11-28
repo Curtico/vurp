d2fee4393c782fc6bdd866235830ed3fabc86632
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2023-11-28 12:24:54 -0500

    removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import ret2win
-import threading
-
-def bruh(binary):
-    print(f"{binary}: {ret2win.exploit(binary)}")
-
-for i in range(10):
-    binary = f"bin-ret2win-{i}"
-    threading.Thread(target=bruh, args=(binary,)).start()
